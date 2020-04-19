f9e87d167b6cc5f53e5b354c20153c702f62bf63
1885189 <1885189@sms.ed.ac.uk>
2020-04-19 18:58:40 +0200

    fixing css

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,13 +9,14 @@ module.exports = {
     devtool: 'source-map',
     stats: 'verbose',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader",
+        }, {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'], // loaders are chainable from right to left
+        }]
     },
     plugins: [
         new HtmlWebPackPlugin({
