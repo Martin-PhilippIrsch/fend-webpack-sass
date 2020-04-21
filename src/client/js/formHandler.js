5164679789ed2227331e44549f3149e1afd7788b
1885189 <1885189@sms.ed.ac.uk>
2020-04-21 19:26:30 +0200

    workbox plugin included

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,6 +1,11 @@
 function handleSubmit(event) {
     event.preventDefault()
 
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
+        // zip={zip code},{country code}
+    const apiKey = '&appid=b1e4acaf20b9ba652fd4325adbc2cac0';
+    let exampleRequestUS = `${baseURL}zip=95616,us${apiKey}`;
+
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
@@ -12,6 +17,25 @@ function handleSubmit(event) {
         .then(function(res) {
             document.getElementById('results').innerHTML = res.message
         })
+
+    fetch(exampleRequestUS)
+        .then(alert(data.main.temp))
+
+    // const getWeatherData = async() => {
+
+    //     const res = await fetch(exampleRequestUS)
+
+    //     try {
+    //         const data = await res.json();
+    //         console.log("Weather data for Davis, CA, received!")
+    //             // console.log(data);
+    //         console.log(data.main.temp)
+    //         return data;
+    //     } catch (error) {
+    //         console.log("error", error);
+    //     };
+    // };
+
 }
 
 export { handleSubmit }
